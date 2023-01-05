2232c80870f1c44696aaf969f6bfde077d7e482a
gusev76 <gusev76dn@mail.ru>
2023-01-05 20:03:53 +0300

    добавили файл task_2.py

---
+++ task_2.py
@@ -0,0 +1,9 @@
+# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N в виде списка. 
+
+num = int(input())
+sum_dig = 1
+num_list = []
+
+for i in range(num):
+    sum_dig *= i + 1
+    print(num_list)
